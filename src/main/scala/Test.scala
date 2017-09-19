2148afe0469a8e1fb39cdf949fa6c3a94d5b58e0
U436938 <markus.maass@dlh.de>
2017-09-19 11:02:24 +0200

    neuere Stringfunktion

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -5,13 +5,14 @@ object Test {
 
   def main(args: Array[String]): Unit = {
     println(stringfunct())
-
+    println(stringfunct1("Hello Markus"))
   }
 
   def stringfunct() :String={
   val str ="Hello World"
   return str
   }
+
   def stringfunct1(neuerName:String) :String={
     return neuerName
   }
