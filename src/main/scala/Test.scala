c7cedf47b39620d49f551d24fa966144e1ff92ed
weikybeidos <wei@kybeidos.de>
2017-09-19 08:03:07 +0200

    basic feature

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -5,14 +5,7 @@ object Test {
 
   def main(args: Array[String]): Unit = {
     println("Hello World")
-    /*doc for complex math function*/
-    var sum = 0
-    for (num <- 1 to 100 if num < 10) {
-      println(num)
-      sum = sum + num
 
-    }
-    println("sum" + sum)
   }
 
 }
