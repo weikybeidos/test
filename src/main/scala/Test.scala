7a64e710a913fe871d87c9793b498199867d5535
userdlh2 <irgendwas@sonstwas.de>
2017-09-19 10:55:00 +0200

    NVI: wilde Kommentare eingefügt

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -5,11 +5,16 @@ object Test {
 
   def main(args: Array[String]): Unit = {
     println(stringfunct())
-
+    println(concat())
+    //ich kommentiere hier mal wild rum
   }
 
   def stringfunct() :String={
-  val str ="Hello World"
+  val str ="Hello World. "
   return str
+  }
+  def concat() :String={
+    val str2= stringfunct() + "I'm feelin fine!"
+    return str2
   }
 }
