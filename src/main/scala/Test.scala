16e6d18f6b95cb670024e6a25a01f2a7705cabbf
weikybeidos <wei@kybeidos.de>
2017-09-19 08:00:35 +0200

    comment added

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -2,16 +2,17 @@
   * Created by bastian on 18.09.2017.
   */
 object Test {
+
   def main(args: Array[String]): Unit = {
     println("Hello World")
-var sum=0
-    for (num <- 1 to 100 if num < 10)
-    {
+    /*doc for complex math function*/
+    var sum = 0
+    for (num <- 1 to 100 if num < 10) {
       println(num)
-      sum =sum + num
+      sum = sum + num
 
     }
-    println("sum"+sum)
+    println("sum" + sum)
   }
 
 }
